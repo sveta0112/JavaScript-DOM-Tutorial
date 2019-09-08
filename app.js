2a64d9ae76e178935fb87201970ecdc9848aad66
sveta0112 <sveta0112@hotmail.com>
2019-09-08 15:08:56 -0700

    hiding book list with click on checkbox

--- app.js
+++ app.js
@@ -165,4 +165,14 @@ addForm.addEventListener('submit', (e) => {
     list.appendChild(li);
 })
 
+//hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', (e) => {
+    if(hideBox.checked) {
+        list.style.display = 'none';
+    } else {
+        list.style.display = 'block';
+    }
+})
+
 
