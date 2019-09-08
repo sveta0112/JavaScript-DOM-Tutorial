7e6fb9746404cd38a1caf39af521339b00cbfded
sveta0112 <sveta0112@hotmail.com>
2019-09-08 12:56:58 -0700

    changing text& edit HTML

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-//titles is a NodeList, not an array, so it does not have access to many array methods
+//titles is a HTML Collection, not an array, so it does not have access to many array methods
 let titles = document.getElementsByClassName('title');
+console.log(titles)
 
 
 //console.log(Array.isArray(titles))//returns false;
-//console.log(Array.isArray(Array.from(titles)))//returns true, because we converted NodeList to Array
+//console.log(Array.isArray(Array.from(titles)))//returns true, because we converted HTMLCollection to Array
 
-//Use Array.from(titles) --> to convert into Array from NodeList
+//Use Array.from(titles) --> to convert into Array from HTMLCollection
 //now we can use forEach method on it, since its Array now
 Array.from(titles).forEach(item => {
     console.log(item);
@@ -15,3 +16,42 @@ Array.from(titles).forEach(item => {
 // which is second child  of ul of div wiht id book-list
 const wmf = document.querySelector('#book-list li:nth-child(2) .name');
 console.log(wmf)//<span class="name">The Wise Man's Fear</span>
+
+//when we use querySelectorAll(), returns NodeList, which has forEach() method, so no need to convert
+const books = document.querySelectorAll('#book-list li .name');
+console.log(books)
+books.forEach(book => {
+    console.log(book.textContent)//just text retrieve
+    //appending (book title) word to each text
+    book.textContent += ' (book title)'
+})
+
+const bookList = document.querySelector('#book-list');
+console.log(bookList.innerHTML)
+/** innerHTML
+ *<h2 class="title">Books to Read</h2>
+	    	<ul>
+	    		<li>
+	    			<span class="name">Name of the Wind (book title)</span>
+	    			<span class="delete">delete</span>
+	    		</li>
+	    		<li>
+	    			<span class="name">The Wise Man's Fear (book title)</span>
+	    			<span class="delete">delete</span>
+	    		</li>
+	    		<li>
+	    			<span class="name">Kafka on the Shore (book title)</span>
+	    			<span class="delete">delete</span>
+	    		</li>
+	    		<li>
+	    			<span class="name">The Master and the Margarita (book title)</span>
+	    			<span class="delete">delete</span>
+	    		</li>
+	    	</ul>
+
+ *
+ *
+ */
+
+ //bookList.innerHTML = '<h2>Books and more books...</h2>'
+ bookList.innerHTML += '<p> This is how you add HTML</p>'
