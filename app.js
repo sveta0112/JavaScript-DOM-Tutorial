91d927ab2e42e2649f9a2172263734e24c1cece4
sveta0112 <sveta0112@hotmail.com>
2019-09-08 15:55:20 -0700

    adding tabbed content

--- app.js
+++ app.js
@@ -194,4 +194,22 @@ hideBox.addEventListener('change', (e) => {
  })
 
 
+ /**--------------------------TABBED CONTENT------------------------ */
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelector('.panel');
+
+tabs.addEventListener('clcik',(e) => {
+    if(e.target.tagName == 'LI'){
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        Array.from(panels).forEach((panel) => {
+          if(panel == targetPanel){
+            panel.classList.add('active');
+          }else{
+            panel.classList.remove('active');
+          }
+        });
+    }
+})
+
+
 
